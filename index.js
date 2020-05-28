2c26659748e6b3a5e3690c949ae672d7e806a6f3
apkil <64503136+apkil@users.noreply.github.com>
2020-05-28 14:27:01 -0300

    mm

--- index.js
+++ index.js
@@ -67,19 +67,105 @@ app.post("/descricoes", (req, res) => {        //Usando body-parser ou query?
 
 
 //Recebe dados do Front?? GENÉRICO Alterado para testar sem as respostas do front
-app.get("/salvaravaliacao",(req,res) =>{  //app.post
+app.get("/avaliacao/utilizacao",(req,res) =>{  //app.post
     var Form_id = "Avaliador"; //req.body.Form_id;
     var Cost_center_id = "CC1"; //req.body.Cost_center_id;
-    var Question_id_answer = {utilização: 1, limpeza: 2}; //req.body.Question_id_answer;
+    var Question_id_answer_u = {utilizacao: 5}; //req.body.Question_id_answer;
 
     res.send("Nota recebida! Form_id:" + Form_id + " Centro de Custo: " + Cost_center_id + "notas: " + Question_id_answer);
     //Envia a nota e descrição para o banco de dados
     Avaliacao.create({
         Form_id: Form_id,
         Cost_center_id: Cost_center_id,
-        Question_id_answer: Question_id_answer,
+        Question_id_answer: Question_id_answer_u,
     });
 });
+
+app.get("/avaliacao/organizacao",(req,res) =>{  //app.post
+    var Form_id = "Avaliador"; //req.body.Form_id;
+    var Cost_center_id = "CC1"; //req.body.Cost_center_id;
+    var Question_id_answer_o = {organizacao: 5}; //req.body.Question_id_answer;
+
+    res.send("Nota recebida! Form_id:" + Form_id + " Centro de Custo: " + Cost_center_id + "notas: " + Question_id_answer);
+    //Envia a nota e descrição para o banco de dados
+    Avaliacao.create({
+        Form_id: Form_id,
+        Cost_center_id: Cost_center_id,
+        Question_id_answer: Question_id_answer_o,
+    });
+});
+
+app.get("/avaliacao/limpeza",(req,res) =>{  //app.post
+    var Form_id = "Avaliador"; //req.body.Form_id;
+    var Cost_center_id = "CC1"; //req.body.Cost_center_id;
+    var Question_id_answer_l = {limpeza: 5}; //req.body.Question_id_answer;
+
+    res.send("Nota recebida! Form_id:" + Form_id + " Centro de Custo: " + Cost_center_id + "notas: " + Question_id_answer);
+    //Envia a nota e descrição para o banco de dados
+    Avaliacao.create({
+        Form_id: Form_id,
+        Cost_center_id: Cost_center_id,
+        Question_id_answer: Question_id_answer_l,
+    });
+});
+
+app.get("/avaliacao/padronizacao",(req,res) =>{  //app.post
+    var Form_id = "Avaliador"; //req.body.Form_id;
+    var Cost_center_id = "CC1"; //req.body.Cost_center_id;
+    var Question_id_answer_p = {padronizacao: 5}; //req.body.Question_id_answer;
+
+    res.send("Nota recebida! Form_id:" + Form_id + " Centro de Custo: " + Cost_center_id + "notas: " + Question_id_answer);
+    //Envia a nota e descrição para o banco de dados
+    Avaliacao.create({
+        Form_id: Form_id,
+        Cost_center_id: Cost_center_id,
+        Question_id_answer: Question_id_answer_p,
+    });
+});
+
+app.get("/avaliacao/disciplina",(req,res) =>{  //app.post
+    var Form_id = "Avaliador"; //req.body.Form_id;
+    var Cost_center_id = "CC1"; //req.body.Cost_center_id;
+    var Question_id_answer_d = {disciplina: 5}; //req.body.Question_id_answer;
+
+    res.send("Nota recebida! Form_id:" + Form_id + " Centro de Custo: " + Cost_center_id + "notas: " + Question_id_answer);
+    //Envia a nota e descrição para o banco de dados
+    Avaliacao.create({
+        Form_id: Form_id,
+        Cost_center_id: Cost_center_id,
+        Question_id_answer: Question_id_answer_d,
+    });
+});
+
+var Answer_average_u = 1;
+var Answer_average_o = 2;
+var Answer_average_l = 3;
+var Answer_average_p = 4;
+var Answer_average_d = 5;
+
+var Answer_average_5s = (Answer_average_u + Answer_average_o + Answer_average_l + Answer_average_p + Answer_average_d)/5;
+var Answer_average_3s = (Answer_average_u + Answer_average_o + Answer_average_l)/3;
+
+var Question_id_answer = {
+    Question_id_answer_u: Question_id_answer_u,
+    Question_id_answer_o: Question_id_answer_o,
+    Question_id_answer_l: Question_id_answer_l,
+    Question_id_answer_p: Question_id_answer_p,
+    Question_id_answer_d: Question_id_answer_d
+};
+
+var Answer_average = {
+    Answer_average_u : Answer_average_u,
+    Answer_average_o : Answer_average_o,
+    Answer_average_l : Answer_average_l,
+    Answer_average_p : Answer_average_p,
+    Answer_average_d : Answer_average_d,
+    Answer_average_3s : Answer_average_3s,
+    Answer_average_5s : Answer_average_5s
+};
+
+console.log(Answer_average_5s+" "+ Answer_average_3s);
+
 
 //Envia dados pro Front?? GENÉRICO
 app.get("/resultado",(req,res) => {
