0b793ef240f5289b51010bd3ca2d43d7defb005c
apkil <64503136+apkil@users.noreply.github.com>
2020-05-28 13:45:49 -0300

    m

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ const connection = require("./database/database");
 const bodyParser = require("body-parser");
 const Avaliacao = require("./database/Avaliacao");
 const Login = require("./database/Login");
-const Descricoes = require("./database/Descricoes");
-const Pergunta = require("./database/Perguntas");
 
 //Body Parser
 app.use(bodyParser.urlencoded({extended:false}));
