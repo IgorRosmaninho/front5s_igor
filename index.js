5ab5ec5acb87f0ecca3cec264a49858e0845b5a3
apkil <64503136+apkil@users.noreply.github.com>
2020-06-09 15:34:36 -0300

    rotas de recebimento do back e calculo da media

--- index.js
+++ index.js
@@ -2,111 +2,11 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+//import {AppRegistry} from 'react-native';
+//import App from './App';
+//import {name as appName} from './app.json';
 
-<<<<<<< HEAD
-AppRegistry.registerComponent(appName, () => App);
-
-// const express = require("express");
-// const app = express();
-// const connection = require("./database/database");
-// const bodyParser = require("body-parser");
-// const Avaliacao = require("./database/Avaliacao");
-// const Login = require("./database/Login")
-
-// //Body Parser
-// app.use(bodyParser.urlencoded({extended:false}));
-// app.use(bodyParser.json());
-
-// //Conexão com Banco de Dados mysql
-// connection.authenticate()
-//     .then(function() {
-//         console.log("Conectado com o BD")
-//     })
-//     .catch(function(err) {
-//         console.log("msgErro");
-//     })
-//     .done();
-
-// //Teste de resposta no Servidor
-// app.get("/",function(req,res){
-//     res.send("Página Principal");
-// });
-
-
-// //Login5
-// app.post("/login", (req, res) => {        //Usando body-parser ou query?
-//     var User_name = req.body.User_name;    //Na requisição deve ser buscado o nome que consta no formulário
-//     var User_password = req.body.User_password;
-//     var User_email = req.body.User_email;
-//     var User_profile_photo = req.body.User_profile_photo;
-//     var User_role = req.body.User_role;
-
-//     //Colocando os dados na tabela de login
-//     Login.create({             
-//         User_name: User_name,
-//         User_password: User_password,
-//         User_email: User_email,
-//         User_profile_photo: User_profile_photo,
-//         User_role: User_role
-//     });                                           //.then para dar um redirect do back para o front
-// }); 
-
-// //Descricoes
-// app.post("/descricoes", (req, res) => {        //Usando body-parser ou query?
-//     var Descricao_id = req.body.Descricao_id
-//     var Descricao = req.body.Descricao;
-
-//     //Colocando os dados na tabela de login
-//     Descricoes.create({             
-//         Descricao: Descricao,
-//         Descricao_id: Descricao_id
-//     });                                           //.then para dar um redirect do back para o front
-// }); 
-
-
-// //Recebe dados do Front?? GENÉRICO Alterado para testar sem as respostas do front
-// app.get("/salvaravaliacao",(req,res) =>{  //app.post
-//     var Form_id = "Avaliador"; //req.body.Form_id;
-//     var Cost_center_id = "CC1"; //req.body.Cost_center_id;
-//     var Question_id_answer = {utilização: 1, limpeza: 2}; //req.body.Question_id_answer;
-
-//     res.send("Nota recebida! Form_id:" + Form_id + " Centro de Custo: " + Cost_center_id + "notas: " + Question_id_answer);
-//     //Envia a nota e descrição para o banco de dados
-//     Avaliacao.create({
-//         Form_id: Form_id,
-//         Cost_center_id: Cost_center_id,
-//         Question_id_answer: Question_id_answer,
-//     });
-// });
-
-// //Envia dados pro Front?? GENÉRICO
-// app.get("/resultado",(req,res) => {
-//     Avaliacao.findAll( {raw: true, order:[
-//         ['id','DESC'] //DESC = decrescente || ASC = crescente
-//     ]}).then(avaliacao => {
-
-//     res.json({             /*Manda todas as notas da avaliação para o front,
-//                             em ordem (mais recente primeiro).
-//                             No front, iremos Usar fetch aqui?*/
-//         avaliacao: avaliacao
-//         });
-    
-//     }); 
-// });
-
-// //Servidor
-// app.listen(4000,function(erro){
-//     if(erro){
-//         console.log("Ocorreu um erro!");
-//     }else{
-//         console.log("Servidor iniciado com sucesso!");
-//     }
-// })
-=======
-AppRegistry.registerComponent(appName, () => App);*/
+//AppRegistry.registerComponent(appName, () => App);
 
 const express = require("express");
 const app = express();
@@ -114,6 +14,11 @@ const connection = require("./database/database");
 const bodyParser = require("body-parser");
 const Avaliacao = require("./database/Avaliacao");
 const Login = require("./database/Login");
+
+//match.js
+const { create, all } = require('mathjs')
+const config = { }
+const math = create(all, config)
 
 //Body Parser
 app.use(bodyParser.urlencoded({extended:false}));
@@ -129,10 +34,6 @@ connection.authenticate()
     })
     .done();
 
-//Teste de resposta no Servidor
-app.get("/",function(req,res){
-    res.send("Página Principal");
-});
 
 //Recebendo dados do login e salvando no BD   
 app.post("/cadastro", (req, res) => {   
@@ -173,138 +74,129 @@ app.get("/login", (req, res) => {
 });
 
 
-//Descricoes
-app.post("/descricoes", (req, res) => {        //Usando body-parser ou query?
-    var Descricao_id = req.body.Descricao_id
-    var Descricao = req.body.Descricao;
-});
-
-app.get("/avaliacao",(req,res) =>{  //app.post
-    var Form_id = "Avaliador1"; //req.body.Form_id;
-    var Cost_center_id = "CC1"; //req.body.Cost_center_id;
-    var Question_id_answer_ = {
-        Question_id_answer_u: 1,
-        Question_id_answer_o: 3,
-        Question_id_answer_l: 4,
-        Question_id_answer_p: 2,
-        Question_id_answer_d: 5}; //req.body.Question_id_answer;
-
-    res.send("Nota recebida! Form_id:" + Form_id + " Centro de Custo: " + Cost_center_id + "notas: " + Question_id_answer);
-    //Envia a nota e descrição para o banco de dados
-    Avaliacao.create({
-        Form_id: Form_id,
-        Cost_center_id: Cost_center_id,
-        Question_id_answer: Question_id_answer_u,
-    });
-});
-
-
-//Recebe dados do Front?? GENÉRICO Alterado para testar sem as respostas do front
-app.get("/avaliacao/utilizacao",(req,res) =>{  //app.post
-    var Form_id = "Avaliador"; //req.body.Form_id;
-    var Cost_center_id = "CC1"; //req.body.Cost_center_id;
-    var Question_id_answer_u = {utilizacao: 5}; //req.body.Question_id_answer;
-
-    res.send("Nota recebida! Form_id:" + Form_id + " Centro de Custo: " + Cost_center_id + "notas: " + Question_id_answer);
-    //Envia a nota e descrição para o banco de dados
-    Avaliacao.create({
-        Form_id: Form_id,
-        Cost_center_id: Cost_center_id,
-        Question_id_answer: Question_id_answer_u,
-    });
-});
-
-app.get("/avaliacao/organizacao",(req,res) =>{  //app.post
-    var Form_id = "Avaliador"; //req.body.Form_id;
-    var Cost_center_id = "CC1"; //req.body.Cost_center_id;
-    var Question_id_answer_o = {organizacao: 5}; //req.body.Question_id_answer;
-
-    res.send("Nota recebida! Form_id:" + Form_id + " Centro de Custo: " + Cost_center_id + "notas: " + Question_id_answer);
-    //Envia a nota e descrição para o banco de dados
-    Avaliacao.create({
-        Form_id: Form_id,
-        Cost_center_id: Cost_center_id,
-        Question_id_answer: Question_id_answer_o,
-    });
-});
-
-app.get("/avaliacao/limpeza",(req,res) =>{  //app.post
-    var Form_id = "Avaliador"; //req.body.Form_id;
-    var Cost_center_id = "CC1"; //req.body.Cost_center_id;
-    var Question_id_answer_l = {limpeza: 5}; //req.body.Question_id_answer;
-
-    res.send("Nota recebida! Form_id:" + Form_id + " Centro de Custo: " + Cost_center_id + "notas: " + Question_id_answer);
-    //Envia a nota e descrição para o banco de dados
-    Avaliacao.create({
-        Form_id: Form_id,
-        Cost_center_id: Cost_center_id,
-        Question_id_answer: Question_id_answer_l,
-    });
-});
-
-app.get("/avaliacao/padronizacao",(req,res) =>{  //app.post
-    var Form_id = "Avaliador"; //req.body.Form_id;
-    var Cost_center_id = "CC1"; //req.body.Cost_center_id;
-    var Question_id_answer_p = {padronizacao: 5}; //req.body.Question_id_answer;
-
-    res.send("Nota recebida! Form_id:" + Form_id + " Centro de Custo: " + Cost_center_id + "notas: " + Question_id_answer);
-    //Envia a nota e descrição para o banco de dados
-    Avaliacao.create({
-        Form_id: Form_id,
-        Cost_center_id: Cost_center_id,
-        Question_id_answer: Question_id_answer_p,
-    });
-});
-
-
-app.get("/avaliacao/disciplina",(req,res) =>{  //app.post
-    var Form_id = "Avaliador"; //req.body.Form_id;
-    var Cost_center_id = "CC1"; //req.body.Cost_center_id;
-    var Question_id_answer_d = {disciplina: 5}; //req.body.Question_id_answer;
-    res.send("Nota recebida! Form_id:" + Form_id + " Centro de Custo: " + Cost_center_id + "notas: " + Question_id_answer_d);
-});
-
-/*
-var Answer_average_u = 1;
-var Answer_average_o = 2;
-var Answer_average_l = 3;
-var Answer_average_p = 4;
-var Answer_average_d = 5;
-
-var Answer_average_5s = (Answer_average_u + Answer_average_o + Answer_average_l + Answer_average_p + Answer_average_d)/5;
-var Answer_average_3s = (Answer_average_u + Answer_average_o + Answer_average_l)/3;
+//define Form_id, Cost_center_id ,Question_id_answer (separar em 3 variáveis)
 
 var Question_id_answer = {
-    Question_id_answer_u: Question_id_answer_u,
-    Question_id_answer_o: Question_id_answer_o,
-    Question_id_answer_l: Question_id_answer_l,
-    Question_id_answer_p: Question_id_answer_p,
-    Question_id_answer_d: Question_id_answer_d
-};
-
-var Answer_average = {
-    Answer_average_u : Answer_average_u,
-    Answer_average_o : Answer_average_o,
-    Answer_average_l : Answer_average_l,
-    Answer_average_p : Answer_average_p,
-    Answer_average_d : Answer_average_d,
-    Answer_average_3s : Answer_average_3s,
-    Answer_average_5s : Answer_average_5s
-};
+    Form_id: 0 ,
+    Cost_center_id: 0 ,
+    Question_id_answer_u: 0,
+    Question_id_answer_o: 0,
+    Question_id_answer_l: 0,
+    Question_id_answer_p: 0,
+    Question_id_answer_d: 0 
+}
 
 
-console.log(Answer_average_5s+" "+ Answer_average_3s);
+//Recebe dados do Front (utilização) e armazena no Json (Question_id_answer)
+app.post("/avaliacao/utilizacao",(req,res) =>{  //app.post
+    var Form_id = req.body.Form_id;
+    var Cost_center_id = req.body.Cost_center_id;
+    var Question_id_answer_u = req.body.Question_id_answer_u;
 
+    Question_id_answer.Form_id = Form_id;
+    Question_id_answer.Cost_center_id = Cost_center_id;
+    Question_id_answer.Question_id_answer_u = Question_id_answer_u
 
-
-Avaliacao.create({
-    Form_id: Form_id,
-    Cost_center_id: Cost_center_id,
-    Question_id_answer: Question_id_answer,
-    Answer_average: Answer_average
+    res.send(Question_id_answer)
+    //res.send("Nota recebida! Form_id:" + Form_id + " Centro de Custo: " + Cost_center_id + " notas: " + Question_id_answer_u);
 });
 
-*/
+//Recebe dados do Front (organização) e armazena no Json (Question_id_answer)
+app.post("/avaliacao/organizacao",(req,res) =>{  //app.post
+    var Form_id = req.body.Form_id;
+    var Cost_center_id = req.body.Cost_center_id;
+    var Question_id_answer_o = req.body.Question_id_answer_o;
+
+    Question_id_answer.Question_id_answer_o = Question_id_answer_o
+
+    res.send(Question_id_answer)
+    //res.send("Nota recebida! Form_id:" + Form_id + " Centro de Custo: " + Cost_center_id + "notas: " + Question_id_answer_o);
+});
+
+//Recebe dados do Front (limpeza) e armazena no Json (Question_id_answer)
+app.post("/avaliacao/limpeza",(req,res) =>{  //app.post
+    var Form_id = req.body.Form_id;
+    var Cost_center_id = req.body.Cost_center_id;
+    var Question_id_answer_l = req.body.Question_id_answer_l;
+
+    Question_id_answer.Question_id_answer_l = Question_id_answer_l
+
+    res.send(Question_id_answer)
+    //res.send("Nota recebida! Form_id:" + Form_id + " Centro de Custo: " + Cost_center_id + "notas: " + Question_id_answer_l);
+
+});
+
+//Recebe dados do Front (padronização) e armazena no Json (Question_id_answer)
+app.post("/avaliacao/padronizacao",(req,res) =>{  //app.post
+    var Form_id = req.body.Form_id;
+    var Cost_center_id = req.body.Cost_center_id;
+    var Question_id_answer_p = req.body.Question_id_answer_p;
+
+    Question_id_answer.Question_id_answer_p = Question_id_answer_p
+
+    res.send(Question_id_answer)
+    //res.send("Nota recebida! Form_id:" + Form_id + " Centro de Custo: " + Cost_center_id + "notas: " + Question_id_answer_p);
+
+});
+
+//Recebe dados do Front (disciplina) e armazena no Json (Question_id_answer)
+app.post("/avaliacao/disciplina",(req,res) =>{  //app.post
+    var Form_id = req.body.Form_id;
+    var Cost_center_id = req.body.Cost_center_id;
+    var Question_id_answer_d = req.body.Question_id_answer_d;
+    
+    Question_id_answer.Question_id_answer_d = Question_id_answer_d
+
+    res.send(Question_id_answer)
+    //res.send("Nota recebida! Form_id:" + Form_id + " Centro de Custo: " + Cost_center_id + "notas: " + Question_id_answer_d);
+});
+
+var Answer_average = {
+    Answer_average_u : 0,
+    Answer_average_o : 0,
+    Answer_average_l : 0,
+    Answer_average_p : 0,
+    Answer_average_d : 0,
+    Answer_average_3s : 0,
+    Answer_average_5s : 0
+};
+
+app.post("/calculamedia",(req,res) =>{
+    var Answer_average_u = math.mean(Question_id_answer.Question_id_answer_u);
+    var Answer_average_o = math.mean(Question_id_answer.Question_id_answer_o);
+    var Answer_average_l = math.mean(Question_id_answer.Question_id_answer_l);
+    var Answer_average_p = math.mean(Question_id_answer.Question_id_answer_p);
+    var Answer_average_d = math.mean(Question_id_answer.Question_id_answer_d);
+
+    Answer_average.Answer_average_u = Answer_average_u
+    Answer_average.Answer_average_o = Answer_average_o
+    Answer_average.Answer_average_l = Answer_average_l
+    Answer_average.Answer_average_p = Answer_average_p
+    Answer_average.Answer_average_d = Answer_average_d
+    Answer_average.Answer_average_3s = math.mean(Answer_average_u,Answer_average_o,Answer_average_l)
+    Answer_average.Answer_average_5s = math.mean(Answer_average_u,Answer_average_o,Answer_average_l,Answer_average_p,Answer_average_d)
+
+    res.send(Answer_average)
+});
+
+app.post("/salvabd", (req,res) => {
+    Avaliacao.create({
+        //Form_id: Form_id,
+    //Cost_center_id: Cost_center_id,
+        Question_id_answer: Question_id_answer,
+        Answer_average: Answer_average
+    });
+    res.send("enviado com sucesso")
+});
+
+
+ 
+app.get("/teste",(req,res) => {
+    //res.send("nota 5.5: " + Question_id_answer.Question_id_answer_d[0] + Question_id_answer.Question_id_answer_d[0] )
+    res.send("nota 5.5: " )
+});
+
+
 
 //Envia dados pro Front?? GENÉRICO
 app.get("/resultado",(req,res) => {
@@ -329,4 +221,3 @@ app.listen(4000,function(erro){
         console.log("Servidor iniciado com sucesso!");
     }
 })
->>>>>>> bb1e18d798a0b9df6959d79dd54f8b4f5bd87b15
