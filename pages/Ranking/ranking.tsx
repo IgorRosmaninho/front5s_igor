d76c98ee84d75e3c8fd83e61f71f48b787d3d438
marinautsunomiya <54456816+marinautsunomiya@users.noreply.github.com>
2020-06-08 23:52:30 -0300

    bottom tab

---
+++ pages/Ranking/ranking.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import {
+    View,
+    Image,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    StyleSheet,
+    FlatList,
+    Button,
+  } from 'react-native';
+
+
+
+  export default function Ranking({navigation}) {
+   
+    
+
+    
+       return (
+         <View style={{flex: 0.2, flexDirection: "column"}}>
+            <Text>Ranking</Text>
+             </View>
+       )}
