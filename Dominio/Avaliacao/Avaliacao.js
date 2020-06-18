d122505aaebdcd24459c844c381e60422ef85afb
apkil <64503136+apkil@users.noreply.github.com>
2020-06-18 18:41:34 -0300

    mm

--- Dominio/Avaliacao/Avaliacao.js
+++ Dominio/Avaliacao/Avaliacao.js
@@ -61,10 +61,11 @@ const Avaliacao = connection.define('avaliacoe',{
 
 });
 
+/*
 //Sincroniza com banco de dados e cria só se não houver uma tabela avaliaçoe
 Avaliacao.sync({force: false}).then(() => {
     console.log("Tabela Avaliacao Criada")
 });
-
+*/
 
 module.exports = Avaliacao;
